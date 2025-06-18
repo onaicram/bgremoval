6679d14ccc2be11844154885c5f52d1336ca05a7
Amedeo Marciano <amedeo.marciano@enel.com>
2025-06-18 17:53:42 +0200

    prima versione

---
+++ src/main/java/com/deus/bgremoval/App.java
@@ -0,0 +1,13 @@
+package com.deus.bgremoval;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
